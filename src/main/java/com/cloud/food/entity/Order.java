1e394a9d3ba2b9d6ed23164ac4b40e86ebfec720
tengyd <tengyd@hd100.com>
2018-03-23 11:01:43 +0800

    添加新属性

--- src/main/java/com/cloud/food/entity/Order.java
+++ src/main/java/com/cloud/food/entity/Order.java
@@ -21,7 +21,86 @@ public class Order {
     private Integer orderStatus;
     private Integer payStatus;
 
-    private Date craeteTime2;
+    private Date createTime;
+    private Date updatTime;
 
+    public String getOrderId() {
+        return orderId;
+    }
 
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getBuyerName() {
+        return buyerName;
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.buyerName = buyerName;
+    }
+
+    public String getBuyerPhone() {
+        return buyerPhone;
+    }
+
+    public void setBuyerPhone(String buyerPhone) {
+        this.buyerPhone = buyerPhone;
+    }
+
+    public String getBuyerAddress() {
+        return buyerAddress;
+    }
+
+    public void setBuyerAddress(String buyerAddress) {
+        this.buyerAddress = buyerAddress;
+    }
+
+    public String getBuyerOpenid() {
+        return buyerOpenid;
+    }
+
+    public void setBuyerOpenid(String buyerOpenid) {
+        this.buyerOpenid = buyerOpenid;
+    }
+
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getPayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(Integer payStatus) {
+        this.payStatus = payStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdatTime() {
+        return updatTime;
+    }
+
+    public void setUpdatTime(Date updatTime) {
+        this.updatTime = updatTime;
+    }
 }
