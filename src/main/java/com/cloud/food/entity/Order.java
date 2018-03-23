6d127f3bb26e5e7ad846f68479b403e962d35970
tengyd <tengyd@hd100.com>
2018-03-23 10:45:23 +0800

    dd

---
+++ src/main/java/com/cloud/food/entity/Order.java
@@ -0,0 +1,27 @@
+package com.cloud.food.entity;
+
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@DynamicUpdate
+public class Order {
+
+
+    private String orderId;
+    private String buyerName;
+    private String buyerPhone;
+    private String buyerAddress;
+    private String buyerOpenid;
+    private BigDecimal orderPrice;
+    private Integer orderStatus;
+    private Integer payStatus;
+
+    private Date craeteTime;
+
+
+}
